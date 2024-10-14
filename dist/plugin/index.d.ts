418de9561a11e3cb58213196996686f0a4f6921b
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2024-10-14 11:32:11 -0700

    build

--- dist/plugin/index.d.ts
+++ dist/plugin/index.d.ts
@@ -1,7 +1,6 @@
-import type { App } from 'vue';
-import type { GlobalOptions } from './types';
-import './styles/main.scss';
-import VResizeDrawer from './VResizeDrawer.vue';
+import { App } from 'vue';
+import { GlobalOptions } from './types';
+import { default as VResizeDrawer } from './VResizeDrawer.vue';
 export declare const globalOptions: unique symbol;
 export declare function createVResizeDrawer(options?: GlobalOptions): {
     install: (app: App) => void;
