49241db7edc663788188dfa051a6b81c538c7acf
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2026-05-02 14:54:32 -0400

    remove dist from commits

--- dist/plugin/composables/helpers.d.ts
+++
@@ -1,4 +0,0 @@
-import { Props, UseConvertToUnit } from '../types';
-export declare const useConvertToUnit: UseConvertToUnit;
-export declare const useConvertToNumber: (val: string | number) => number;
-export declare const useUnitToPx: (valueWithUnit: Props["handleIconSize"]) => number;
