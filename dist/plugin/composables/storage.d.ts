9fd6222f6a9911ec4499c8852f78f73a1960b6c8
WebDevNerdStuff <webdevnerdstuff@gmail.com>
2026-05-02 15:32:52 -0400

    update ignores

--- dist/plugin/composables/storage.d.ts
+++
@@ -1,3 +0,0 @@
-import { UseSetStorage } from '../types';
-export declare function useGetStorage(storageType: string, storageName: string): string | null;
-export declare const useSetStorage: UseSetStorage;
--- dist/plugin/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { GlobalOptions } from './types';
-import { App } from 'vue';
-import { default as VResizeDrawer } from './VResizeDrawer.vue';
-export declare const globalOptions: unique symbol;
-export declare function createVResizeDrawer(options?: GlobalOptions): {
-    install: (app: App) => void;
-};
-export default VResizeDrawer;
-export { VResizeDrawer, };
